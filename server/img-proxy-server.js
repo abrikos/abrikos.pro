1eda4058c6bbb814f51e9c0c21771d77f3b6b779
abrikos <abrikoz@gmail.com>
2019-12-17 11:15:07 +0900

    img proxy

--- server/img-proxy-server.js
+++ server/img-proxy-server.js
@@ -3,9 +3,10 @@ const axios = require("axios");
 //create a server object:
 http.createServer(function (req, res) {
 
-    console.log(req.url.substr(1))
+
     axios.get(req.url.substr(1))
         .then(r=>{
+            console.log(r.data)
             res.writeHead(200, {'Content-Type': 'image/jpeg'});
             res.write(r.data);
             res.end(); //end the response
