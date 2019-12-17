ee2e11461d4fcca7ec04bc466ef861c366d9c1ca
abrikos <abrikoz@gmail.com>
2019-12-17 11:12:14 +0900

    img proxy

--- server/img-proxy-server.js
+++ server/img-proxy-server.js
@@ -3,11 +3,12 @@ const axios = require("axios");
 //create a server object:
 http.createServer(function (req, res) {
 
-
+    console.log(req.url.substr(1))
     axios.get(req.url.substr(1))
         .then(r=>{
             res.writeHead(200, {'Content-Type': 'image/jpeg'});
             res.write(r.data);
             res.end(); //end the response
         })
+        .catch(e=>console.log(e.message))
 }).listen(8080); //the server object listens on port 8080
