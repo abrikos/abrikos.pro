dc1cbbc1967c7bce765e67bea8f53a6f66553c15
abrikos <abrikoz@gmail.com>
2020-05-05 14:47:44 +0900

    portfolio

--- client/sites/main/portfolio.js
+++ client/sites/main/portfolio.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import {A, useRoutes} from "hookrouter";
-import logo from "client/images/logo.png";
+
 export default function Portfolio(props) {
 
+    const sites = [
+        {url: 'https://www.beru14.ru', description: 'Сайт бесплатных объявлений'},
+        {url: 'http://iltumen.ru/', description: 'Государственное собрание (ИЛ ТУМЭН) РС(Я)'},
+        {url: 'http://sakhaenergo.ru/', description: 'АО «Сахаэнерго»(ОАО «Сахаэнерго»)'},
+        {url: 'https://kadry.sakha.gov.ru/', description: 'Кадровый портал РС(Я)'},
+        {url: 'https://yakutia.science/', description: 'Академия Наук РС(Я)'}
+    ]
     return <div className={'text-center'}>
         <h1>Портфолио</h1>
-        <div><a href={"https://www.beru14.ru"} target={'_blank'}>Сайт бесплатных объявлений</a></div>
-        <div><a href={"http://purchases.abrikos.pro/"} target={'_blank'}>Сервис управления списками закупок</a></div>
+        {sites.map((s,i)=><div key={i}><a href={s.url} target={'_blank'}>{s.description}</a></div>)}
+
+
+        {/*<div><a href={"http://purchases.abrikos.pro/"} target={'_blank'}>Сервис управления списками закупок</a></div>*/}
         {/*<div><a href={"https://svg.abrikos.pro"} target={'_blank'}>Эксперименты с SVG</a></div>*/}
-        <div><a href={"https://minesweeper.abrikos.pro"} target={'_blank'}>Классическая игра "Сапер"</a></div>
+        {/*<div><a href={"https://minesweeper.abrikos.pro"} target={'_blank'}>Классическая игра "Сапер"</a></div>
         <div><a href={"https://minter-earth.com"} target={'_blank'}>Гео визитки</a></div>
         <div><a href={"https://telegram.me/AbrikosLottery_bot"} target={'_blank'}>Бот крипто лотерей</a></div>
-        <div><a href={"http://minter-cities.abrikos.pro/"} target={'_blank'}>Города где живут Минтерианцы</a></div>
+        <div><a href={"http://minter-cities.abrikos.pro/"} target={'_blank'}>Города где живут Минтерианцы</a></div>*/}
     </div>
 
 }
